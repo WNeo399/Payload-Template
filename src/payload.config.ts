27c129b56a6c820b7a533c49d289efbd28668dfc
WNeo399 <wneo399@gmail.com>
2026-01-09 13:02:52 +1100

    update

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -58,6 +58,9 @@ export default buildConfig({
   // This config helps us configure global or default features that the other editors can inherit
   editor: defaultLexical,
   db: mongooseAdapter({
+    connectOptions: {
+      dbName: process.env.dbName || 'TemplateDB',
+    },
     url: process.env.DATABASE_URL || '',
   }),
   collections: [Pages, Posts, Media, Categories, Users],
